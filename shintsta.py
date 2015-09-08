ff81515b3be5597c5b4b14c7c9254c8715fc8e4f
Sergio <svalqui@839L-83211-W.unimelb.edu.au>
2015-09-08 16:59:52 +1000

    Testing; sh ver

--- shintsta.py
+++ shintsta.py
@@ -4,21 +4,26 @@
 # Created : 2015/08/13
 # Modified : 2015/0813
 
-from netmiko import ConnetHandler
+from netmiko import ConnectHandler
+import getpass
 
+gs_DeviceName = input('DeviceName: ')
+gs_UserName = getpass.getpass("Username: ")
+gs_password = getpass.getpass()
+gs_EnablePass = getpass.getpass("Enabled Password: ")
 
 
 cisco_881 = {
     'device_type': 'cisco_ios',
-    'ip':   '',
-    'username': '',
-    'password': '',
-    'port' : 22,          # optional, defaults to 22
-    'secret': '',     # optional, defaults to ''
+    'ip':   gs_DeviceName,
+    'username': gs_UserName,
+    'password': gs_password,
+    #'port' : 22,          # optional, defaults to 22
+    'secret': gs_EnablePass,     # optional, defaults to ''
     #'verbose': False,       # optional, defaults to True
  }
 
-net_connect = ConnetHandler(**cisco_881)
+net_connect = ConnectHandler(**cisco_881)
 
 output = net_connect.send_command('sh ver')
 print(output)
