dfb90eda2bd60041de1f910159cd2231aec84ede
Sergio <svalqui@839L-83211-W.unimelb.edu.au>
2015-10-09 17:25:13 +1100

    Progressing with class definitions

--- NetDef.py
+++ NetDef.py
@@ -6,13 +6,12 @@
 
 class NetworkDevice:
     """ Class container for all attributes and methods related to a Network Device """
-    def __init__(self, DeviceName, UserName, UPassword, EnablePassword):
+    def __init__(self, device_name, user_name, user_password, enable_password):
         """ Initializing containers"""
-        self.DeviceName = DeviceName
-        self.UserName = UserName
-        self.UPassword = UPassword
-        self.EnablePassword = EnablePassword
-        self.Connected = False
+        self.DeviceName = device_name
+        self.UserName = user_name
+        self.UPassword = user_password
+        self.EnablePassword = enable_password
         self.Interfaces = {}
         self.Vlans = {}
         self.Modules = []
@@ -20,9 +19,8 @@ class NetworkDevice:
         self.ShIntStatus = {}
         self.VRF = {}
 
-    def GetConnected(self, self.DeviceName, self.UserName, self.UPassword, self.EnablePassword):
         from netmiko import ConnectHandler
-        Cisco_Device = {
+        self.Cisco_Device = {
             'device_type': 'cisco_ios',
             'ip': self.DeviceName,
             'username': self.UserName,
@@ -31,7 +29,15 @@ class NetworkDevice:
             'secret': self.EnablePassword,     # optional, defaults to ''
             #'verbose': False,       # optional, defaults to True
             }
-        Device_Connetion = ConnectHandler(**Cisco_Device)
+        self.Device_Connetion = ConnectHandler(**self.Cisco_Device)
+
+    def SendCommand(self, command):
+        output = self.Device_Connetion.send_command(command)
+        return(output)
+
+    def Disconnect(self):
+        self.Device_Connetion.disconnect()
+
 
 
 
--- shintsta.py
+++ shintsta.py
@@ -6,3 +6,40 @@
 
 
 
+import getpass
+import NetDef
+
+gs_DeviceName = input('DeviceName: ')
+gs_UserName = getpass.getpass("Username: ")
+gs_password = getpass.getpass()
+gs_EnablePass = getpass.getpass("Enabled Password: ")
+
+
+switch1 = NetDef.NetworkDevice(gs_DeviceName, gs_UserName, gs_password, gs_EnablePass)
+
+fs_keyinput = ''
+
+while fs_keyinput != 'q':
+    fs_keyinput = input()
+    if fs_keyinput != "":
+        output = switch1.SendCommand(fs_keyinput)
+        print(output)
+    else:
+        try:
+            exec(fs_keyinput)
+        except NameError :
+            print('NameError')
+        except SyntaxError:
+            print('SyntaxError')
+        except AttributeError:
+            print('AttributeError:')
+        except TypeError:
+            print('TypeError:')
+        except KeyError:
+            print('KeyError:')
+        except ValueError:
+            print('ValueError:')
+
+switch1.Disconnect()
+print('finished for now')
+
