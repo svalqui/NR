d8e79084dc50334f3c8c416c17641103343a96e9
Sergio <svalqui@unimelb.edu.au>
2016-04-11 17:25:26 +1000

    checking enabled mode use

--- exa020ios-upg.py
+++ exa020ios-upg.py
@@ -16,6 +16,13 @@ gs_EnablePass = getpass.getpass("Enabled Password: ")
 
 switch1 = netdef.NetworkDevice(gs_DeviceName, gs_UserName, gs_password, gs_EnablePass)
 
+print("In Enabled Mode? : ",switch1.Device_Connection.check_enable_mode)
+
+if switch1.Device_Connection.check_enable_mode:
+    print("In enabled mode")
+else:
+    switch1.Device_Connection.enable()
+
 # Working with the IOS version, getting it and presenting a brief.
 print("getting sh ver...")
 switch1.show_version()
