02eaa9866c2dfa8d3ced490e605aab1786f0db90
Sergio <svalqui@unimelb.edu.au>
2016-04-22 12:16:55 +1000

    reorganizing files, new library needed for managing files.

---
+++ libfilesio.py
@@ -0,0 +1,8 @@
+# A library to work with files to read and write text files, common uses for NetworkTangents only.
+#
+# Authors: Sergio Valqui
+# Created : rebuilt from 2013
+# Modified : 2016/
+
+def ltxtf(filename, show_progress= False):
+    import os
