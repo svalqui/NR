5a62f8fbf48c2cc6f5983a6b824682fa34a6bd0a
Sergio <svalqui@unimelb.edu.au>
2016-05-04 11:51:12 +1000

    progressing write text file function with common options.

--- libfilesio.py
+++ libfilesio.py
@@ -5,10 +5,10 @@
 # Modified : 2016/
 
 
-def l_text_f(path_and_filename, show_progress= False):
+def l_text_f(path_and_filename, show_progress=False):
     import pathlib
     status = 0  # 0 : Good file exist with data; 1: file empty; 2: file do not exists
-    content_return = []
+    content = []
     if pathlib.Path.exists(path_and_filename):  # if the file exists
         file_obj = pathlib.Path.open(path_and_filename, 'r')
         content_lines = file_obj.readlines()
@@ -21,10 +21,35 @@ def l_text_f(path_and_filename, show_progress= False):
                 print("File EMPTY")
         else:  # if has lines
             for line in content_lines:
-                content_return.append(line.rstrip("\n"))  # strip returns
+                content.append(line.rstrip("\n"))  # strip returns
     else:
         if show_progress:
             print(str(path_and_filename))
             print("File DO NOT Exists")
             status = 2
-    return status, content_return
+    return status, content
+
+
+def w_text_file(path_and_filename, content, overwrite=False, create_copy=False, debug=False):
+    import pathlib
+    if pathlib.Path.exists(path_and_filename):
+        if overwrite:  # Write
+            file_obj = open(path_and_filename, 'w')
+        elif create_copy:  # Copy and Write
+            import shutil
+            import datetime
+            now = datetime.datetime.now().strftime("-%Y%m%d-%H%M%S")
+
+        else:  # Append
+            file_obj = open(path_and_filename, 'a')
+    else:  # Write
+        file_obj = open(path_and_filename, 'w')
+
+    for line in content:
+        if type(line).__name__ == "str":
+            file_obj.write(line + '\n')
+        else:
+            file_obj.write(str(line) + '\n')
+    file_obj.close()
+
+    return
