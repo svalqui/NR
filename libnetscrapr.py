47ee80eb753070436d6245fca9a1f125fa3f4f74
Sergio <svalqui@unimelb.edu.au>
2016-08-09 17:49:36 +1000

    creating library to work with monitoring systems, scrapers/ REST api querys.

---
+++ libnetscrapr.py
@@ -0,0 +1,18 @@
+# libnetrestapi library for interactions with REST api systems
+#  Cisco Prime, Statseeker, Infoblox
+# Author: Sergio Valqui
+# Created : 2015/08/09
+
+from bs4 import BeautifulSoup
+import getpass
+import requests
+from requests.auth import HTTPBasicAuth
+
+UserName = getpass.getpass("Username: ")
+password = getpass.getpass()
+url = input()
+
+page = requests.get(url, verify=False, auth=HTTPBasicAuth(gs_UserName, gs_password))
+print (page.status_code)
+print ("============")
+print(page.text)
