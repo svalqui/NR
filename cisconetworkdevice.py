a85e000b673b3ed287a3bce67ae38a745e66609f
Sergio <svalqui@unimelb.edu.au>
2016-08-31 11:47:13 +1000

    reorganizing file structure and content

--- cisconetworkdevice.py
+++ cisconetworkdevice.py
@@ -5,59 +5,7 @@
 # Modified : 2016/
 
 from lib import netconparser
-
-
-class Interface(object):
-    """Class container for all attributes and methods related to an Interface, they are part of NetworkDevice"""
-    def __init__(self):
-        self.InterfaceName = ''
-        self.InterfaceShortName = ''
-        self.ShowInterfacePerInt = []
-        self.ShowInterfaceSwitchportPerInt = []
-        self.ShowRunningConfigurationPerInt = []
-        self.ShowInterfaceCapabilitiesPerInt = []
-        self.InterfaceDescription = ''
-        self.PacketsInput = ''
-        self.PacketsOutput = ''
-        self.LineProtocol = ''
-        self.InputErrors = ''
-        self.OutputErrors = ''
-        self.LastClearing = ''
-        self.AdministrativeMode = ''
-        self.AccessModeVlan = ''
-        self.VoiceVlan = ''
-        self.Type = ''
-
-    def load_interface_details(self):
-        """
-        fills in class details coming from 'sh int'
-        and 'sh int switchport' both should be already filled
-        :param:
-        :return:
-        """
-        for line in self.ShowInterfacePerInt:
-            if line.find('Description:') >= 0:
-                self.InterfaceDescription = line.replace('Description:', '')
-            elif line.find('packets input') >= 0:
-                self.PacketsInput = int(line.split()[0])
-            elif line.find('packets output') >= 0:
-                self.PacketsOutput = int(line.split()[0])
-            elif line.find('line protocol') >= 0:
-                self.LineProtocol = line
-            elif line.find('Last clearing of') >= 0:
-                self.LastClearing = line.split()[-1]
-
-        for line in self.ShowInterfaceSwitchportPerInt:
-            if line.find('Administrative Mode:') >= 0:
-                self.AdministrativeMode = line[19:]
-            elif line.find('Access Mode VLAN:') >= 0:
-                self.AccessModeVlan = line.split()[3]
-            elif line.find('Voice VLAN:') >= 0:
-                self.VoiceVlan = line.split()[2]
-
-        for line in self.ShowInterfaceCapabilitiesPerInt:
-            if line.find('Type:') >= 0:
-                self.Type = line.split(":")[1].strip()
+import ciscointerface
 
 
 class NetworkDevice(object):
--- interface.py
+++ interface.py
@@ -1,1 +1,24 @@
 # Interface Source Class
+
+class Interface(object):
+    """Class container for all attributes and methods related to an Interface, they are part of NetworkDevice"""
+    def __init__(self):
+        self.InterfaceName = ''
+        self.InterfaceShortName = ''
+        self.InterfaceDescription = ''
+        self.PacketsInput = ''
+        self.PacketsOutput = ''
+        self.InputErrors = ''
+        self.OutputErrors = ''
+        self.Type = ''
+
+    def load_interface_details(self):
+        """
+        fills in class details coming from 'sh int'
+        and 'sh int switchport' both should be already filled
+        :param:
+        :return:
+        """
+        return
+
+
