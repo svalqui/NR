e1fedceb64e5276c19d04dacfdb077478808f5a7
Sergio <svalqui@unimelb.edu.au>
2016-09-13 17:38:18 +1000

    progressing reseting unreachables, and adding cgis for the web functionality .

--- cgi-bin/cgi-w-reset-unreac.py
+++ cgi-bin/cgi-w-reset-unreac.py
@@ -21,7 +21,7 @@ dict_devices_interfaces = {}
 print("Content-Type: text/html;charset=utf-8\n")
 print("<pre>")
 
-class_cisco_prime = cpriapi.CiscoPrimeApi(user, password)
+class_cisco_prime = cispriapi.CiscoPrimeApi(user, password)
 list_ap_cdp, list_ap_no_cdp = class_cisco_prime.list_unreachable_neighbors()
 
 for item in list_ap_cdp:
@@ -35,35 +35,10 @@ for item in list_ap_cdp:
 print("ready for resetting:")
 
 for item in list_devices:
-    print("  ", item, " :",dict_devices_interfaces[item])
+    print("  ", item, " :", dict_devices_interfaces[item])
 
 print("---")
 print(datetime.datetime.today().strftime(date_format))
 print(sys.version)
 print("Hasta la vista baby...............")
 print("</pre>")
-
-
-
-
-
-
-class_cisco_prime = cpriapi.CiscoPrimeApi(user_name, password)
-
-list_ap_cdp, list_ap_no_cdp = class_cisco_prime.list_unreachable_neighbors()
-
-list_devices = []
-dict_devices_interfaces = {}
-
-for item in list_ap_cdp:
-    if item[1] not in list_devices:
-        list_devices.append(item[1])
-    list_devices.sort()
-    if item[1] not in dict_devices_interfaces:
-        dict_devices_interfaces[item[1]] = []
-    dict_devices_interfaces[item[1]].append(item[2])
-
-print("ready for resetting:")
-
-for item in list_devices:
-    print("  ", item, " :",dict_devices_interfaces[item])
