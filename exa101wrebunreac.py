a4e96650a1fcd5106ee49bf0c4a05d1a1223e35f
Sergio <svalqui@unimelb.edu.au>
2016-09-16 12:04:36 +1000

    capturing connection exceptions on example.,

--- exa101wrebunreac.py
+++ exa101wrebunreac.py
@@ -8,6 +8,7 @@
 import getpass
 import lib.restapi.ciscoprimeapi as cpriapi
 from networktangents import cisconetworkdevice
+import sys
 
 user_name = getpass.getpass("Username: ")
 password = getpass.getpass()
@@ -22,7 +23,9 @@ list_ap_cdp, list_ap_no_cdp = class_cisco_prime.list_unreachable_neighbors()
 list_devices = []
 dict_devices_interfaces = {}
 
+print("ready for resetting:")
 for item in list_ap_cdp:
+    print(item)
     if item[1] not in list_devices:
         list_devices.append(item[1])
     list_devices.sort()
@@ -30,7 +33,7 @@ for item in list_ap_cdp:
         dict_devices_interfaces[item[1]] = []
     dict_devices_interfaces[item[1]].append(item[2])
 
-print("ready for resetting:")
+print()
 
 for item in list_devices:
     print("  ", item, " :", dict_devices_interfaces[item])
@@ -39,6 +42,7 @@ enable_password = getpass.getpass("Enable Password :")
 
 for item in list_devices:
     print("Connecting to :", item)
+    connected = False
 
     try:
         network_device = cisconetworkdevice.CiscoNetworkDevice(item, user_name, password, enable_password)
@@ -47,12 +51,15 @@ for item in list_devices:
         line_log = "Could NOT connect to: " + network_device + " Possible empty/unknown character in file"
         print(line_log)
     except:
-        line_log = "Error: " + sys.exc_info()[0]
-        print(line_log)
+        line_log = "Error: "
+        print(line_log, sys.exc_info()[0])
+        print(line_log, sys.exc_info()[1])
+        print(line_log, sys.exc_info()[2], "\n")
 
     if connected:
         print("Resetting :", dict_devices_interfaces[item])
         network_device.reset_interfaces(dict_devices_interfaces[item])
         network_device.disconnect()
         print("  finished with: ", item, "\n")
+
 
