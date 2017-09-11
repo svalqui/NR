b3b3df27f47745d2e5c65873bd0fd26f2bbec1f1
svalqui <svalqui@unimelb.edu.au>
2017-09-11 12:11:35 +1000

    Finished exa001

--- exa001connect.py
+++ exa001connect.py
@@ -4,12 +4,14 @@
 # #
 # Authors: Sergio Valqui
 # Created : 2017/02
-# Modified : 2017/02
+# Modified : 2017/09
 
 import getpass
-from datetime import datetime
 from netmiko import ConnectHandler
+import time
 import telnetlib
+
+# Device Name, Username and pwd
 
 device_name = input('DeviceName or IP: ')
 user_name = getpass.getpass("Username: ")
@@ -17,33 +19,54 @@ password = getpass.getpass()
 
 # Using telnetlib
 
+print("\n***Using telnetlib***\n")
+
 print("Connecting to: ", device_name, " using telnetlib")
 telnet_connection = telnetlib.Telnet(device_name)
 
 telnet_connection.read_until(b"Username: ")
 telnet_connection.write(user_name.encode('ascii') + b"\n")
+#print("Passing username")
 if password:
     telnet_connection.read_until(b"Password: ")
     telnet_connection.write(password.encode('ascii') + b"\n")
+#    print("Passing pw")
+#    print(telnet_connection.read_all().decode('ascii'))
 
-telnet_connection.write(b"sh ver\n")
-telnet_connection.write(b"exit\n")
+value = telnet_connection.expect([b'>', b'Authentication '], 15)
 
-print(telnet_connection.read_all().decode('ascii'))
+if value[0] == 0:
+    print("Logged in ", device_name)
+    telnet_connection.write(b"terminal length 0\n")
+    telnet_connection.write(b"sh ver\n")
+    time.sleep(2)
+    telnet_connection.write(b"exit\n")
+    print(telnet_connection.read_all().decode('ascii'))
+
+elif value[0] == 1:
+    print("Authentication failure")
 
 telnet_connection.close()
 
 # Using Netmiko
 
-Net_Device = {
-    'device_type': device_type,
+print("\n***Using netmiko***\n")
+
+network_device = {
+    'device_type': 'cisco_ios',
     'ip': device_name,
     'username': user_name,
     'password': password,
 }
 
-Device_Connection = ConnectHandler(**self.Cisco_Device)
+device_connection = ConnectHandler(**network_device)
 
+output = device_connection.send_command("sh ver")
+print(output)
 
-# Reference http://stackoverflow.com/questions/19671936/telnet-cisco-switch-using-python
+# Credits Disclaimer
+# The below sites/articles/code has been used totally, partially or as reference
+#
+# http://stackoverflow.com/questions/19671936/telnet-cisco-switch-using-python
 # https://docs.python.org/3.3/library/telnetlib.html
+# https://github.com/ktbyers/netmiko
